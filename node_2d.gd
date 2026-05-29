c4d06d44cea953f373bf3287c5ef0018a064d7a6
skilly <tnt3267690242@gmail.com>
2026-05-29 15:18:12 +0800

    地图拖动完善

--- node_2d.gd
+++ node_2d.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 @onready var camera = $Camera2D
+@onready var background = $TextureRect
 
 var dragging = false
 var last_mouse_pos = Vector2.ZERO
+
+func _ready():
+	var bg_rect = background.get_global_rect()
+	camera.position = bg_rect.position + bg_rect.size / 2.0
+	_clamp_camera()
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
@@ -17,4 +23,14 @@ func _unhandled_input(event):
 	if event is InputEventMouseMotion and dragging:
 		var delta = event.position - last_mouse_pos
 		camera.position -= delta
+		_clamp_camera()
 		last_mouse_pos = event.position
+
+func _clamp_camera():
+	var viewport_size = get_viewport_rect().size
+	var half_view = viewport_size / 2.0
+	var bg_rect = background.get_global_rect()
+	var min_pos = bg_rect.position + half_view
+	var max_pos = bg_rect.end - half_view
+	camera.position.x = clampf(camera.position.x, min_pos.x, max_pos.x)
+	camera.position.y = clampf(camera.position.y, min_pos.y, max_pos.y)
